743b26cdb3415729f70713dbed6a8f809528dce0
phamhoangminh2410it <125272514+phamhoangminh2410it@users.noreply.github.com>
2023-03-06 09:35:36 +0700

    Add files via upload

--- BaiTapLab4.java
+++ BaiTapLab4.java
@@ -2,11 +2,13 @@ class Circle
 {
 	private double radius;
 	private String color;
+	private double area;
 
-	public Circle(double radius, String color)
+	public Circle(double radius, String color, double area)
 	{
 		this.radius = radius;
 		this.color = color;
+		this.area = area;
 	}
 	public double getRadius()
 	{
@@ -16,9 +18,9 @@ class Circle
 	{
 		return this.color;
 	}
-	public double getArea(double area)
+	public double getArea()
 	{
-		return area;
+		return this.area;
 	}
 	public String toString()
 	{
@@ -30,9 +32,9 @@ class Cylinder extends Circle
 {
 	private double height;
 
-	public Cylinder(double radius, String color, double height)
+	public Cylinder(double radius, String color, double area, double height)
 	{
-		super(radius, color);
+		super(radius, color, area);
 		this.height = height;
 	}
 	public void setHeight(double height)
@@ -53,8 +55,8 @@ public class BaiTapLab4
 {
 	public static void main(String[] args)
 	{
-		Cylinder data1 = new Cylinder(1.2, "Red", 2.5);
-		data1.getArea(5);
+		Cylinder data1 = new Cylinder(1.2, "Red", 2.5, 3.2);
 		System.out.println(data1.toString());
+		System.out.println(data1.getVolume());
 	}
 }
